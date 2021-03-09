4f271e3ad090a082894ce2561ae2e362c54f0d22
txs1992 <xusheng.txs@gmail.com>
2021-03-09 08:17:14 +0800

    feature：处理 attribute

--- src/clients/html-parser.js
+++ src/clients/html-parser.js
@@ -1,11 +1,18 @@
 const EOF = Symbol("EOF"); // 文件结束 End of file
+let currentToken = null;
+
+function emit(token) {
+  console.log(token);
+}
 
 function data(c) {
   if (c === "<") {
     return tagOpen;
   } else if (c === EOF) {
+    emit({ type: "EOF" });
     return;
   } else {
+    emit({ type: "text", content: c });
     return data;
   }
 }
@@ -14,6 +21,7 @@ function tagOpen(c) {
   if (c === "/") {
     return endTagOpen;
   } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = { type: "startTag", tagName: "" };
     return tagName(c);
   } else {
     return;
@@ -22,7 +30,11 @@ function tagOpen(c) {
 
 function endTagOpen(c) {
   if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = { type: "endTag", tagName: "" };
     return tagName(c);
+  } else if (c === ">") {
+  } else if (c === EOF) {
+  } else {
   }
 }
 
@@ -32,6 +44,7 @@ function tagName(c) {
   } else if (c === "/") {
     return selfClosingStartTag;
   } else if (c === ">") {
+    currentToken.tagName += c;
     return data;
   } else {
     return tagName;
@@ -41,14 +54,110 @@ function tagName(c) {
 function beforeAttributeName(c) {
   if (c.match(/^[\t\n\f ]$/)) {
     return beforeAttributeName;
-  } else if (c === ">") {
-    return data;
+  } else if (c === "/" || c === ">" || c === EOF) {
+    return afterAttributeName(c);
   } else if (c === "=") {
     return beforeAttributeName;
   } else {
-    return beforeAttributeName;
+    currentAttribute = {
+      name: "",
+      value: "",
+    };
+    return attributeName(c);
   }
 }
+
+function attributeName(c) {
+  if (c.match(/^[\t\n\f ]$/) || c === "/" || c === ">" || c === EOF) {
+    return afterAttributeName(c);
+  } else if (c === "=") {
+    return beforeAttributeValue;
+  } else if (c === "\u0000") {
+  } else if (c === '"' || c === "'" || c === "<") {
+  } else {
+  }
+}
+
+function afterAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return afterAttributeName;
+  } else if (c === "/") {
+    return selfClosingStartTag;
+  } else if (c === "=") {
+    return beforeAttributeValue;
+  } else if (c === ">") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c === EOF) {
+  } else {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    currentAttribute = {
+      name: "",
+      value: "",
+    };
+    return attributeName(c);
+  }
+}
+
+function beforeAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/) || c === "/" || c === ">" || c === EOF) {
+    return beforeAttributeValue;
+  } else if (c === '"') {
+    return doubleQuotedAttributeValue;
+  } else if (c === "'") {
+    return signleQuotedAttributeValue;
+  } else if (c === ">") {
+  } else {
+    return unquotedAttributeValue(c);
+  }
+}
+
+function doubleQuotedAttributeValue(c) {
+  if (c === '"') {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttributeValue;
+  } else if (c === "\u0000") {
+  } else if (c === EOF) {
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function signleQuotedAttributeValue(c) {
+  if (c === "'") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttributeValue;
+  } else if (c === "\u0000") {
+  } else if (c === EOF) {
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function unquotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return beforeAttributeName;
+  } else if (c === "/") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return selfClosingStartTag;
+  } else if (c === ">") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c === "\u0000") {
+  } else if (c === '"' || c === "'" || c === "<" || c === "=" || c === "`") {
+  } else if (c === EOF) {
+  } else {
+    currentAttribute.value += c;
+    return unquotedAttributeValue;
+  }
+}
+
+function afterQuotedAttributeValue(c) {}
 
 function selfClosingStartTag(c) {
   if (c === ">") {
